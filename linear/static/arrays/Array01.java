8f3ff1486c2e092972086948fc6c0bd86aa8c553
sharankarthikyan <>
2022-08-19 10:39:33 +0530

    1 & 2D Array - Arrays

---
+++ linear/static/arrays/Array01.java
@@ -0,0 +1,27 @@
+public class Array01 {
+
+    public static void printArr(int[] arr, int currentIdx) {
+        if (currentIdx == arr.length) {
+            return;
+        } else {
+            System.out.print(arr[currentIdx] + " ");
+            printArr(arr, currentIdx + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // int array
+        int[] arr1 = { 1, 2, 3, 4, 5 };
+        printArr(arr1, 0);
+
+        System.out.println();
+
+        int[] arr2 = new int[10];
+        arr2[0] = 0;
+        arr2[1] = 1;
+        arr2[2] = 2;
+        arr2[3] = 3;
+        printArr(arr2, 0);
+    }
+}
---
+++ linear/static/arrays/Array02.java
@@ -0,0 +1,24 @@
+public class Array02 {
+    public static int printArr(int[][] arr, int i, int j) {
+        if (i >= arr.length)
+            return 1;
+        if (j >= arr[i].length)
+            return 0;
+
+        System.out.print(arr[i][j] + " ");
+
+        if (printArr(arr, i, j + 1) == 1)
+            return 1;
+
+        return printArr(arr, i + 1, 0);
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {
+                { 1, 2, 3, 4, 5 },
+                { 6, 7, 8, 9, 10 }
+        };
+        printArr(arr1, 0, 0);
+
+    }
+}
